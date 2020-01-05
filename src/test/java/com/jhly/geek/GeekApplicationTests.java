df65fa79b14179eaa08379ac0b7b30feae21d4c7
jhly <13545175547@163.com>
2020-01-05 11:00:45 +0800

    极客时间-玩转spring全家桶

---
+++ src/test/java/com/jhly/geek/GeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhly.geek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
